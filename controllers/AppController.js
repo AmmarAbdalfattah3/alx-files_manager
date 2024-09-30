ff0920d022e8afff0f0492cad27355d7a72b2d47
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-09-30 09:47:47 -0700

    app controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,25 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
-
   static getStatus(req, res) {
-    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+
+    return res.status(200).json({ redis: redisAlive, db: dbAlive });
   }
 
   static async getStats(req, res) {
-    const users = await dbClient.nbUsers();
-    const files = await dbClient.nbFiles();
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
 
-    res.status(200).json({ users, files });
+      return res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      console.error('Error retrieving stats:', error);
+      return res.status(500).json({ error: 'Unable to retrieve stats' });
+    }
   }
 }
 
-module.exports = AppController;
+export default AppController;
