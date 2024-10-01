ad3546689691b816e9fe3d8e6957c6b9a3f2b964
AhmedNewiry <ahmedabdalfttah@outlook.com>
2024-10-01 15:24:05 -0700

    eslint

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,18 @@
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
-    static async getStatus(req, res) {
-        const redisAlive = redisClient.isAlive();
-        const dbAlive = dbClient.isAlive();
-        res.status(200).json({ redis: redisAlive, db: dbAlive });
-    }
+  static async getStatus(req, res) {
+    const redisAlive = redisClient.isAlive();
+    const dbAlive = dbClient.isAlive();
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  }
 
-    static async getStats(req, res) {
-        const usersCount = await dbClient.nbUsers();
-        const filesCount = await dbClient.nbFiles();
-        res.status(200).json({ users: usersCount, files: filesCount });
-    }
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({ users: usersCount, files: filesCount });
+  }
 }
 
 export default AppController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,45 +1,44 @@
-import dbClient from '../utils/db.js';
+import dbClient from '../utils/db';
 import crypto from 'crypto';
 import redisClient from '../utils/redis';
 
 class UsersController {
-    static async postNew(req, res) {
-        const { email, password } = req.body;
+  static async postNew(req, res) {
+    const { email, password } = req.body;
 
-        if (!email) return res.status(400).json({ error: 'Missing email' });
-        if (!password) return res.status(400).json({ error: 'Missing password' });
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
-        const user = await dbClient.findUserByEmail(email);
-        if (user) return res.status(400).json({ error: 'Already exist' });
+    const user = await dbClient.findUserByEmail(email);
+    if (user) return res.status(400).json({ error: 'Already exists' });
 
-        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-        const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+    const result = await dbClient.db.collection('users').insertOne({ email, password: hashedPassword });
 
-        return res.status(201).json({ id: result.insertedId, email });
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    static async getMe(req, res) {
-        const token = req.header('X-Token');
-        if (!token) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
 
-        const key = `auth_${token}`;
-        const userId = await redisClient.get(key);
-
-        if (!userId) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-
-        const user = await dbClient.db.collection('users').findOne({ _id: new dbClient.ObjectID(userId) });
-
-        if (!user) {
-            return res.status(401).json({ error: 'Unauthorized' });
-        }
-
-        return res.status(200).json({ id: user._id, email: user.email });
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
     }
 
+    const user = await dbClient.db.collection('users').findOne({ _id: new dbClient.ObjectID(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
 }
 
 export default UsersController;
