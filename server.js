04d7298e592100655cde886f262aac5d76bce45d
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-10-01 09:14:03 -0700

    server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = require('./routes/index');
+const redisClient = require('./utils/redis.js');
+const dbClient = require('./utils/db.js');
 
 const app = express();
 
