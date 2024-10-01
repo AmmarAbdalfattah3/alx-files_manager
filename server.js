1871f63e409cbaa124c499a5c8cc8d2873fc6db0
AmmarAbdalfattah3 <ammarabdalfattah251@gmail.com>
2024-10-01 10:04:38 -0700

    server

--- server.js
+++ server.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const routes = require('./routes/index.js');
-const redisClient = require('./utils/redis.js');
-const dbClient = require('./utils/db.js');
+const routes = require('./routes/index');
+const redisClient = require('./utils/redis');
+const dbClient = require('./utils/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  const status = {
-    redis: redisClient.isAlive(),
-    db: dbClient.isAlive(),
-  };
-  console.log("Redis and DB Status: ", status);
-  next();
-});
 
 app.use('/', routes);
 
